2d507f69c36174830f06371a532cfd7a76227c70
Steve Dodier-Lazaro <sdodierlazaro@gmail.com>
2020-07-12 10:36:46 +0200

    chore: Tidy up release assets for Github packages

--- release.config.js
+++ release.config.js
@@ -12,7 +12,7 @@ module.exports = {
 		[
 			'@semantic-release/git',
 			{
-				assets: ['build/*', 'CHANGELOG.md', 'package.json', 'yarn.lock'],
+				assets: ['build/*', 'CHANGELOG.md', 'package.json'],
 				message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
 			},
 		],
